ffd0d1ff67ca13c2a12229adcb98de195926acce
Eric Lee <ericlee1995@hotmail.com>
2020-05-21 14:53:21 +0800

    Update resume file

--- routes/resume.js
+++ routes/resume.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-const fileName = "files/resume.pdf";
+const fileName = "files/Eric_Lee_Resume.pdf";
 
 router.get("/", function (req, res, next) {
 	res.download(fileName, function (err) {
