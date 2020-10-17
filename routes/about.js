f1940cf95c221bb4c81d4f9ad0b839defd87ae37
Eric Lee <ericlee1995@hotmail.com>
2020-10-17 00:09:26 -0700

    Mask MongoDB URI

--- routes/about.js
+++ routes/about.js
@@ -1,19 +1,18 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-var MongoClient = require("mongodb").MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 
-const uri =
-	"mongodb+srv://ericlee:lcPBtSuBrg4k3M5b@cluster0-xandl.gcp.mongodb.net/test?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
 
-router.get("/", (req, res, next) => {
+router.get('/', (req, res, next) => {
 	client.connect((err) => {
 		if (err) throw err;
 
-		const collection = client.db("eric").collection("workExperience");
+		const collection = client.db('eric').collection('workExperience');
 
 		collection.find().toArray((err, result) => {
 			if (err) throw err;
